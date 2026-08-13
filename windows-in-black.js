d364cb96422368778e0b205279b3d1ec139ab9ab
NoctisNexus <tilejani76@gmx.de>
2026-08-13 20:35:58 +0200

    Update windows-in-black.js

--- windows-in-black.js
+++ windows-in-black.js
@@ -19,9 +19,9 @@ const windowsSongs = [
         title: "Behind the Mask",
         file: "musik/Behind the Mask.mp3"
     },
-   {
+ {
     title: "In to the Grey",
-    file: "https://raw.githubusercontent.com/NoctisNexus/FallenAngels/refs/heads/main/musik/in-to-the-grey.mp3"
+    file: "musik/in-to-the-grey.mp3"
 }
 ];
 const audio =
